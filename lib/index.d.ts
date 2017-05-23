66f642049663db62d02bab95a208e3bcd765f99e
Mitch Crowe <crowe.mitch@gmail.com>
2017-05-23 15:50:56 -0700

    Release v0.0.1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,31 @@
-declare function doSomething(a: number): number;
-export default doSomething;
+export interface IEnvelope {
+    attack: number;
+    sustain: number;
+    decay: number;
+    fade: number;
+    release: number;
+}
+export interface IPatch {
+    envelope: IEnvelope;
+    harmonics: number[];
+}
+export declare const PIANO_PATCH: {
+    envelope: {
+        attack: number;
+        sustain: number;
+        decay: number;
+        fade: number;
+        release: number;
+    };
+    harmonics: number[];
+};
+declare class Synth {
+    patch: IPatch;
+    ctx: AudioContext;
+    constructor(patch: IPatch);
+    play(freq: number, gain: number): void;
+    private playHarmonic(freq, gain);
+    private createSineOscillatorNode(freq);
+    private createEnvelopeGainNode(gain);
+}
+export default Synth;
